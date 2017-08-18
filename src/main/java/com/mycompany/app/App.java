33bb18b350062a992529bf3bcf86423b1546aef0
Arne <arne.schueler@gmail.com>
2017-08-19 00:33:27 +0200

    Initial src commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,30 @@
+package com.mycompany.app;
+
+
+import com.beowulfe.hap.HomekitRoot;
+import com.beowulfe.hap.HomekitServer;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	private static final int PORT = 9123;
+	
+	public static void main(String[] args) {
+		try {
+			HomekitServer homekit = new HomekitServer(PORT);
+			HomekitRoot bridge = homekit.createBridge(new MockAuthInfo(), "Test Bridge", "TestBridge, Inc.", "G6", "111abe234");
+			bridge.addAccessory(new MockSwitch());
+			bridge.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+
+
+}
